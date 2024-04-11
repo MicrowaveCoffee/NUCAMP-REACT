474a8023c0949a13340989eaa66d01fb0e45e518
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2024-04-10 21:21:29 -0500

    campsiteSlice

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,19 @@
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image , name } = campsite;
     return (
         <Card>
             <CardImg 
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
+                src={image}
+                alt={name}
             />
-            <CardTitle>{props.campsite.name}</CardTitle> 
+            <CardImgOverlay>
+             <CardTitle>{name}</CardTitle>   
+            </CardImgOverlay>
+             
         </Card>
     )
 }
