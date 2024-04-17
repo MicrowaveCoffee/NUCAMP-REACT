1a680dc822f89caea79bf7707ae7c6fadf43bf1e
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2024-04-16 23:08:36 -0500

    end of week 2 code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage.js'
 import HomePage from './pages/HomePage.js'
+import AboutPage from './pages/AboutPage.js'
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 
 function App() {
@@ -14,8 +16,12 @@ function App() {
             <Header />
             <Routes>
                 <Route path='/' element={<HomePage />} />
+                <Route path='/about' element={<AboutPage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+                
+                
             </Routes>
             <Footer />
     </div>
