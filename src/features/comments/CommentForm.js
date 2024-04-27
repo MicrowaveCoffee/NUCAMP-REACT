dfd3a0303a4ad534350f18944666f70a56198316
MicrowaveCoffee <32424042+MicrowaveCoffee@users.noreply.github.com>
2024-04-27 18:13:29 -0500

    createSlice

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,8 +1,21 @@
 import { useState } from 'react'
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap'
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import {validateCommentForm} from '../../utils/validateCommentForm'
 
 const CommentForm = ({campsiteId}) => {
     const [modalOpen, setModalOpen] = useState(false)
+    const handleSubmit = (values) => {
+        const comment = 
+        {
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        console.log(comment)
+        setModalOpen(false)
+    }
     return (
         <>
             <Button outline onClick = {() => setModalOpen(true)}>
@@ -10,7 +23,54 @@ const CommentForm = ({campsiteId}) => {
             </Button>
             <Modal isOpen={modalOpen}>
                 <ModalHeader toggle = {() => setModalOpen(false)}></ModalHeader>
-                <ModalBody>campsite: {campsiteId}</ModalBody>
+               <Formik initialValues = 
+               {{
+                rating: undefined,
+                author: '',
+                commentText: ''
+                }}
+                onSubmit = {{handleSubmit}}
+                validate={validateCommentForm}>
+                    <Form>
+                        <FormGroup>
+                            <ErrorMessage name = 'rating'>{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
+                            <Label htmlfor='rating'>Rating</Label>
+                            <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                        </FormGroup>
+                        <FormGroup>
+                            <ErrorMessage name = 'author'>{(msg) => <p className='text-danger'>{msg}</p>}</ErrorMessage>
+                            <Label htmlfor= 'author'>Your Name</Label>
+                            <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlfor = 'commentText'>Comment</Label>
+                            <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                        </FormGroup>
+                        <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                    </Form>
+                </Formik>
             </Modal>
         </>
     )
